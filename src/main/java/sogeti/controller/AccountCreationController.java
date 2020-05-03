346e37e4f19b6bc3988cc143a094622a1b8be6c6
Florian <florian.binder@wanadoo.fr>
2020-05-03 12:30:03 +0200

    Création de compte Front+Back, modifs pom.xml et application.properties

--- src/main/java/sogeti/controller/AccountCreationController.java
+++ src/main/java/sogeti/controller/AccountCreationController.java
@@ -1,5 +1,22 @@
 package sogeti.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+
+import sogeti.model.User;
+
+@Controller
 public class AccountCreationController {
+
+    @RequestMapping("/register")
+    public ModelAndView showRegisterUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return new ModelAndView("createaccount.html");
+    }
 
 }
--- src/main/java/sogeti/model/User.java
+++ src/main/java/sogeti/model/User.java
@@ -1,14 +1,38 @@
 package sogeti.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column (name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
-    private String mail;
+
+    @Column (name = "username")
+    private String username;
+
+    @Column (name = "email")
+    private String email;
+
+    @Column (name = "password")
     private String password;
 
 
+    public User(String name, String surname, String username, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public int getId() {
         return id;
@@ -34,12 +58,20 @@ public class User {
         this.surname = surname;
     }
 
-    public String getMail() {
-        return mail;
+    public String getUsername() {
+        return username;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
