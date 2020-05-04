2cf8f830cdc2d6d565d912bf0cdd5187ced11c6d
Benjamin Mouginot <benjamin.mouginot@live.fr>
2020-05-04 10:15:33 +0200

    Revert "Merge branch 'master' into origin/arnaud"
    This reverts commit ff34a4ec555ffe7ca7e42197e1ffe2e12e45758c.

--- src/main/java/sogeti/controller/HomepageController.java
+++ src/main/java/sogeti/controller/HomepageController.java
@@ -1,15 +1,5 @@
 package sogeti.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
 public class HomepageController {
-
-    @GetMapping("/homepage")
-    public ModelAndView showHomepage() {
-        return new ModelAndView("homepage.html");
-    }
 
 }
--- src/main/java/sogeti/model/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package sogeti.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sogeti.model.User;
-
-public interface RoleRepository extends JpaRepository<User, String> {
-
-}
