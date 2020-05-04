57cbc4ae4e693ee89a4a504fe1ecf47a1e89404e
Murat <muratcolak27@gmail.com>
2020-05-04 15:21:41 +0200

    formulaire d'envoi Email

---
+++ src/main/java/sogeti/controller/EmailController.java
@@ -0,0 +1,41 @@
+package sogeti.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@SpringBootApplication
+class Application implements CommandLineRunner {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        System.out.println("Sending Email...");
+        sendEmail();
+        System.out.println("Done");
+
+    }
+
+    void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("hcare67000@gmail.com");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
+}
