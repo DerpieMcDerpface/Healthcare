7ca61691b4cc0d4ed19b7a9453084719ed921f68
Arnaud <arnaud.derbal@gmail.com>
2020-05-04 10:43:21 +0200

    Update UserRepository.java + rename pages

--- src/main/java/sogeti/model/repository/UserRepository.java
+++ src/main/java/sogeti/model/repository/UserRepository.java
@@ -8,4 +8,5 @@ import sogeti.model.User;
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByUsername(String username);
     User findByEmail(String email);
+    User findByPassword(String password);
 }
